85f862298c277c3a71917ebc4a02c5ecda54406c
Dawid Jaroszewicz <jaroszewicz.dawid.kamil@gmail.com>
2023-09-25 21:28:41 +0200

    Add DbContext

--- LogolendarzAuthAPI/Program.cs
+++ LogolendarzAuthAPI/Program.cs
@@ -1,7 +1,17 @@
+using LogolendarzAuthAPI.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+  options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>()
+  .AddDefaultTokenProviders();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -17,9 +27,21 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllers();
 
+app.MapControllers();
+ApplyMifration();
 app.Run();
+void ApplyMifration()
+{
+  using (var scope = app.Services.CreateScope())
+  {
+    var _db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    if (_db.Database.GetPendingMigrations().Count() > 0)
+    {
+      _db.Database.Migrate();
+    }
+  };
+}
